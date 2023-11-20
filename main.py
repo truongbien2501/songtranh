d252a1076152417ddd3fb3a37acb85a34bb12306
truongbien2501 <85795627+truongbien2501@users.noreply.github.com>
2023-11-20 20:52:30 +0700

    update

--- main.py
+++ main.py
@@ -78,21 +78,22 @@ class Hochua(MDApp):
         toast(args[0])
 
     def show_example_grid_bottom_sheet(self):
-        bottom_sheet_menu = MDGridBottomSheet()
-        data = {
-            "Hạn ngắn": "facebook",
-            "Hạn vừa": "youtube",
-            "Hạn dài": "twitter",
-            "Lũ": "cloud",
-            "Cảnh báo lũ": "camera",
-        }
-        for item in data.items():
-            bottom_sheet_menu.add_item(
-                item[0],
-                lambda x, y=item[0]: self.callback_for_menu_items(y),
-                icon_src=item[1],
-            )
-        bottom_sheet_menu.open()
+        pass
+        # bottom_sheet_menu = MDGridBottomSheet()
+        # data = {
+        #     "Hạn ngắn": "facebook",
+        #     "Hạn vừa": "youtube",
+        #     "Hạn dài": "twitter",
+        #     "Lũ": "cloud",
+        #     "Cảnh báo lũ": "camera",
+        # }
+        # for item in data.items():
+        #     bottom_sheet_menu.add_item(
+        #         item[0],
+        #         lambda x, y=item[0]: self.callback_for_menu_items(y),
+        #         icon_src=item[1],
+        #     )
+        # bottom_sheet_menu.open()
     
     def callback_for_menu_items(self, selected_item):
         # Thực hiện cập nhật hình ảnh dựa trên mục được chọn
